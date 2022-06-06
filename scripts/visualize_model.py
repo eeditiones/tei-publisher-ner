efde2621b396d9b128255286b2d91026a8d2e856
Wolfgang Meier <wolfgang@existsolutions.com>
2022-06-06 17:22:58 +0200

    visualize_model: allow model selection

--- scripts/visualize_model.py
+++ scripts/visualize_model.py
@@ -1,10 +1,26 @@
+from typing import Optional
+import spacy
 import spacy_streamlit
 import typer
+import os
+from pathlib import Path
 
+def model_list():
+    dir = Path(__file__).parent.parent / "models"
+    subdirs = os.listdir(dir)
+    models = [Path("models", model, "model-best") for model in subdirs]
+    for pipe in spacy.info()["pipelines"]:
+        models.append(pipe)
+    return models
 
-def main(models: str, default_text: str):
-    models = [name.strip() for name in models.split(",")]
-    spacy_streamlit.visualize(models, default_text, visualizers=["ner"])
+def main(model: str, default_text: str):
+    models = model_list()
+    spacy_streamlit.visualize(
+        models=models, 
+        default_text=default_text,
+        default_model=model,
+        visualizers=["ner"]
+    )
 
 
 if __name__ == "__main__":
